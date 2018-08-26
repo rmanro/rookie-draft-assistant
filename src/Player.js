09da9530707539f58cd27fda86e74c814f1ddc77
Ryan Manro <rmanro@gmail.com>
2018-08-26 12:53:32 -0700

    injury icon

--- src/Player.js
+++ src/Player.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
 import './Player.css';
+import cross from './hospital.png';
 
 class Player extends Component {
 
@@ -10,13 +11,14 @@ class Player extends Component {
   }
   
   render() {
-    const { Rookies, Rank, Pos, color, visible, target, Team } = this.props.player;
+    const { Rookies, Rank, Pos, color, visible, target, Team, IR } = this.props.player;
 
     if (!visible) return null;
 
     return ( 
       <Card className="Players-div" style={{background:color}} onClick={this.handleChangeColor}>
-        <h1>{Rank}. {Rookies} <span className="team">{Team}</span></h1><h2>{Pos}{target && <span className="target"> TARGET</span>}
+        <h1>{Rank}. {Rookies} <span className="team">{Team}</span></h1>
+        <h2>{Pos} {IR && <img src={cross} alt="Injured"/>}{target && <span className="target"> TARGET</span>}
         {color === '#4CAF50' && <strong> - Drafted</strong>}</h2>
       </Card>
     );
